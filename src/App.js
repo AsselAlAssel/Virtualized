417f8acaf1dd0a3a01c358ddcd4b16b29494a13a
Assel Al-Assel <aseel1234500000@gmail.com>
2022-05-25 00:27:38 +0300

    use react-virtualized for faster render for huge list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { List } from "react-virtualized"
 import './App.css';
 
 const App = () => {
@@ -14,17 +15,27 @@ const App = () => {
     const interval = setInterval(() => {
       setTime(new Date);
     }, 1000)
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div>
       <div>hello from me</div>
       <h2>{time.toISOString()}</h2>
-      {
-        data.map((ele, index) => {
-          return <p key={index}>{ele}</p>
-        })
-      }
+      <List
+        width={600}
+        height={600}
+        rowHeight={60}
+        rowCount={data.length}
+        rowRenderer={
+          ({ key, index, style }) => {
+            return <p key={key} style={style}>the index from the data is {index}</p>
+          }
+        }
+      />
+
+
     </div>
   );
 }
